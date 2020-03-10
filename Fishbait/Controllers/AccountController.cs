4fc9046859ffcccbf1d43f89155bf680f710618e
TimonC <58507497+TimonC@users.noreply.github.com>
2020-03-10 17:26:09 +0100

    Database connectie proberen te maken

---
+++ Fishbait/Controllers/AccountController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Fishbait.Models;
+using MySql.Data.MySqlClient;
+
+namespace Fishbait.Controllers
+{
+    public class AccountController : Controller
+    {
+        MySqlConnection con = new MySqlConnection();
+        MySqlCommand com = new MySqlCommand();
+        MySqlDataReader dr;
+        //Get the account
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        void connectionString()
+        {
+            con.ConnectionString = "server=Localhost;user id=Tijmen;database=fishbait";
+        }
+        public IActionResult Verify(User acc)
+        {
+            connectionString();
+            con.Open();
+            com.Connection = con;
+            com.CommandText = "select * from users where username='"+acc.Username+"' and password='"+acc.Password+"'";
+            dr = com.ExecuteReader();
+            if(dr.Read())
+            {
+                con.Close();
+                return View("Succes");
+            }
+            else
+            {
+                con.Close();
+                return View("Failure");
+            }
+          
+   
+        }
+    }
+}
---
+++ Fishbait/Models/Database.cs
@@ -0,0 +1,13 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fishbait.Models
+{
+    public class Database
+    {
+        MySqlConnection con = new MySqlConnection("server=Localhost;user id=Tijmen;database=fishbait");
+    }
+}
